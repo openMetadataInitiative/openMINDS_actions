fa3dadf2b62414efeb3574260a7687de1dac86a1
Raphael-Gazzotti <125291580+Raphael-Gazzotti@users.noreply.github.com>
2026-07-13 09:35:22 +0200

    Fix expected type name handling for plural schemas

--- openMINDS_validation/validation.py
+++ openMINDS_validation/validation.py
@@ -214,10 +214,11 @@ class InstanceValidator(object):
 
         if self._id_schema_name in {'licenses', 'contentTypes', 'accessibilities'}:
             # self._type_schema_name is not using plural
-            if expected_type_name.endswith("ies"):
-                expected_type_name = expected_type_name[:-3] + "y"
-            elif expected_type_name.endswith("s"):
-                expected_type_name = expected_type_name[:-1]
+            if self._id_schema_name.endswith("ies"):
+                expected_type_name = self._id_schema_name[:-3] + "y"
+            elif self._id_schema_name.endswith("s"):
+                expected_type_name = self._id_schema_name[:-1]
+            expected_type_name = expected_type_name[:1].upper() + expected_type_name[1:]
         if expected_type_name != self._type_schema_name:
             logging.error(f'Mismatch between @id schema name "{self._id_schema_name}" and @type schema name "{self._type_schema_name}".')
 
