38ee9bdcbd78062ba8eb175c563c5f3741b196c2
Raphael-Gazzotti <125291580+Raphael-Gazzotti@users.noreply.github.com>
2026-08-14 15:42:09 +0200

    Update repository check to compare last segment of path

--- openMINDS_validation/validation.py
+++ openMINDS_validation/validation.py
@@ -34,7 +34,7 @@ class SchemaTemplateValidator(object):
 
     def check_repository(self):
         latest_modules = self.version_file["latest"]["modules"]
-        if not any(submodule.get("repository") == self.repository for submodule in latest_modules.values()):
+        if not any(submodule.get("repository").split('/')[-1] == self.repository for submodule in latest_modules.values()):
             logging.error(f'Repository "{self.repository}" is not registered in the "latest" module of the openMINDS version file.')
 
     def check_extends(self):
