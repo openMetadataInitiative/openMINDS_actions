6301eebab98371105f2c8b1e268aa9a89d98333e
Raphael-Gazzotti <125291580+Raphael-Gazzotti@users.noreply.github.com>
2026-09-07 17:29:34 +0200

    Fix logging error message for schema template validation

--- openMINDS_validation/validation.py
+++ openMINDS_validation/validation.py
@@ -32,7 +32,7 @@ class SchemaTemplateValidator(object):
         """
         path = PurePath(self.absolute_path)
         if "schemas" in path.parts and not path.name.endswith(".schema.tpl.json"):
-            logging.error(f'Invalid file name "{path.name}": a schema template must end in ".schema.tpl.json")
+            logging.error(f'Invalid file name "{path.name}": a schema template must end in ".schema.tpl.json"')
 
     def check_attype(self):
         """
